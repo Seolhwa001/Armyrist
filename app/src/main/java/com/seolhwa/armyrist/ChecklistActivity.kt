05c3202ad21fdaef6eb4ad920c7455cd2a1e8bcc
Seolhwa001 <anfrhrldj2@gmail.com>
2026-08-11 16:08:28 +0900

    Add files via upload

--- app/src/main/java/com/seolhwa/armyrist/ChecklistActivity.kt
+++ app/src/main/java/com/seolhwa/armyrist/ChecklistActivity.kt
@@ -95,7 +95,7 @@ class ChecklistActivity : ComponentActivity() {
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
