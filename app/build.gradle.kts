e5d15f15889f685146c7d671e8afd1d5fe2bb23a
Seolhwa001 <anfrhrldj2@gmail.com>
2026-08-12 14:32:57 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.seolhwa.armyrist"
         minSdk = 23
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.3.15-pre-timeplan-cleanup"
+        versionCode = 16
+        versionName = "0.3.16-step0-buildfix"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
