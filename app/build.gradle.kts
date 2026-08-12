49af7aa2edc7b547c205bb9f258e78c98d7b4e20
Seolhwa001 <anfrhrldj2@gmail.com>
2026-08-12 16:41:38 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.seolhwa.armyrist"
         minSdk = 23
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.3.19-timeplan-v2-persistence"
+        versionCode = 20
+        versionName = "0.3.20-timeplan-v2-ui"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
