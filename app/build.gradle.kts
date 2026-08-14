bd281ce842a5ec22960119f93c09801ed3148bd2
Seolhwa001 <anfrhrldj2@gmail.com>
2026-08-14 19:57:00 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.seolhwa.armyrist"
         minSdk = 23
         targetSdk = 36
-        versionCode = 51
-        versionName = "0.4.11-voice-contextual-guide"
+        versionCode = 52
+        versionName = "0.4.12-voice-dedicated-recording"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
