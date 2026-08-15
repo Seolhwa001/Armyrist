ef22a30b5949e1030dabd082a93dad9d32840e7d
Seolhwa001 <anfrhrldj2@gmail.com>
2026-08-15 22:39:37 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.seolhwa.armyrist"
         minSdk = 23
         targetSdk = 36
-        versionCode = 67
-        versionName = "0.4.27-minor-patch-b-poc-cleanup"
+        versionCode = 68
+        versionName = "0.4.28-minor-patch-b-poc-cleanup-final"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
