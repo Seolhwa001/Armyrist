76fd981ea4c773f939a7071b9a8b832267fc33b2
Seolhwa001 <anfrhrldj2@gmail.com>
2026-08-23 23:02:38 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.seolhwa.armyrist"
         minSdk = 23
         targetSdk = 36
-        versionCode = 111
-        versionName = "0.6.23-legacy-shadow-trash-back-fix"
+        versionCode = 112
+        versionName = "0.6.24-legacy-import-dateaware-fix"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
