2db863c3d3c5a078db112c84b1575a067cf0d12f
Seolhwa001 <anfrhrldj2@gmail.com>
2026-08-24 04:40:21 +0900

    Add files via upload

--- app/src/main/java/com/seolhwa/armyrist/timeplan/v3/ui/DateAwareTimePlanScreen.kt
+++ app/src/main/java/com/seolhwa/armyrist/timeplan/v3/ui/DateAwareTimePlanScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
