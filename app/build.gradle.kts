1ae8ea5c71742b173db79f12aaa7014992469921
Seolhwa001 <anfrhrldj2@gmail.com>
2026-08-23 18:57:03 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.seolhwa.armyrist"
         minSdk = 23
         targetSdk = 36
-        versionCode = 99
-        versionName = "0.6.11-emergency-midway-delete-stability"
+        versionCode = 100
+        versionName = "0.6.12-atomic-point-delete-ui-unification"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
