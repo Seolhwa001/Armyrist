8fee6e96d52e1f73347f9e94093c7f212fee8cf6
Seolhwa001 <anfrhrldj2@gmail.com>
2026-08-26 00:04:56 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.seolhwa.armyrist"
         minSdk = 23
         targetSdk = 36
-        versionCode = 143
-        versionName = "0.6.55"
+        versionCode = 144
+        versionName = "0.6.56"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
